e70e9edab3a5c7cf4e7ff548ae24ffe979e3480a
unknown <winewig@hotmail.com>
2022-02-17 11:28:48 +0100

    Moved routes from index to App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,55 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Routes, Route, Link, Outlet } from "react-router-dom";
+
+import Expenses from "./routes/expenses";
+import Invoices from "./routes/invoices";
+import Invoice from "./routes/invoice";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Bookkeeper!</h1>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/expenses" element={<Expenses />} />
+          <Route path="/invoices" element={<Invoices />}>
+            <Route
+              index
+              element={
+                <main style={{ padding: "1rem" }}>
+                  <p>Select an invoice</p>
+                </main>
+              }
+            />
+            <Route path="2000" element={<div>2000</div>} />
+            {/* :invoiceId of the path is a "URL param", meaning it can match any 
+          value as long as the pattern is the same */}
+            <Route path=":invoiceId" element={<Invoice />} />
+          </Route>
+          <Route
+            path="*"
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>There's nothing here!</p>
+              </main>
+            }
+          />
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+function Layout() {
+  return (
+    <>
       <nav style={{ borderBottom: "solid 1px", paddingBottom: "1rem" }}>
         <Link to="/invoices">Invoices</Link> |{" "}
         <Link to="/expenses">Expenses</Link>
       </nav>
       <Outlet />
-    </div>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,43 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Expenses from "./routes/expenses";
-import Invoices from "./routes/invoices";
-import Invoice from "./routes/invoice";
 
 ReactDOM.render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route path="/expenses" element={<Expenses />} />
-        <Route path="/invoices" element={<Invoices />}>
-          <Route
-            index
-            element={
-              <main style={{ padding: "1rem" }}>
-                <p>Select an invoice</p>
-              </main>
-            }
-          />
-          <Route path="2000" element={<div>2000</div>} />
-          {/* :invoiceId of the path is a "URL param", meaning it can match any 
-          value as long as the pattern is the same */}
-          <Route path=":invoiceId" element={<Invoice />} />
-        </Route>
-        <Route
-          path="*"
-          element={
-            <main style={{ padding: "1rem" }}>
-              <p>There's nothing here!</p>
-            </main>
-          }
-        />
-      </Route>
-    </Routes>
+    <App />
   </BrowserRouter>,
   document.getElementById("root")
 );
